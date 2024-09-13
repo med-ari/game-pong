2bfb1c9cca0830083760bd293108c90577f34184
arm-jez <138801938+arm-jez@users.noreply.github.com>
2024-09-12 23:57:25 -0300

    3 steps to pong

---
+++ main.lua
@@ -0,0 +1,74 @@
+push = require 'push'
+
+PADDLE_SPEED = 200
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH ,WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = 1
+    })
+    smallFont = love.graphics.newFont("font.ttf", 8)
+    scoreFont = love.graphics.newFont("font.ttf", 32)
+    
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
+    math.randomseed(os.time())
+
+    ballX = VIRTUAL_WIDTH / 2 - 2 
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+    ballDX = 
+    ballDY = 
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
+function love.draw()
+    love.graphics.clear(30/255, 30/255, 46/255, 255/255)
+    push:apply('start')
+    
+    love.graphics.setColor(180/255, 190/255, 254/255)
+    love.graphics.setFont(smallFont)
+    love.graphics.printf("Hola Pongo!", 0, 10, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 8)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 8)
+
+    love.graphics.setColor(249/255, 226/255, 175/255)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5, 20)
+
+    love.graphics.setColor(243/255, 139/255, 168/255)
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 , VIRTUAL_HEIGHT / 2 , 4, 4)
+
+    push:apply('end')
+end
