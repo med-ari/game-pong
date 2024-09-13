26fa4b7848eaa8e14ca7facd75464d8b02131c06
med-ari <131400549+med-ari@users.noreply.github.com>
2024-09-13 00:38:49 -0300

    stage 4 ball update

--- main.lua
+++ main.lua
@@ -10,14 +10,14 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH ,WINDOW_HEIGHT, {
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = 1
     })
     smallFont = love.graphics.newFont("font.ttf", 8)
     scoreFont = love.graphics.newFont("font.ttf", 32)
-    
+
     player1Score = 0
     player2Score = 0
 
@@ -26,15 +26,29 @@ function love.load()
 
     math.randomseed(os.time())
 
-    ballX = VIRTUAL_WIDTH / 2 - 2 
+    ballX = VIRTUAL_WIDTH / 2 - 2
     ballY = VIRTUAL_HEIGHT / 2 - 2
-    ballDX = 
-    ballDY = 
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    gameState = "start"
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == "enter" or key == "return" then
+        if gameState == "start" then
+            gameState = "play"
+        else
+            gameState = "start"
+
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
     end
 end
 
@@ -49,26 +63,36 @@ function love.update(dt)
     elseif love.keyboard.isDown('down') then
         player2Y = player2Y + PADDLE_SPEED * dt
     end
+    if gameState == "play" then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
+    end
 end
 
 function love.draw()
-    love.graphics.clear(30/255, 30/255, 46/255, 255/255)
     push:apply('start')
-    
-    love.graphics.setColor(180/255, 190/255, 254/255)
+
+    love.graphics.clear(30 / 255, 30 / 255, 46 / 255, 255 / 255)
+
+    love.graphics.setColor(180 / 255, 190 / 255, 254 / 255)
     love.graphics.setFont(smallFont)
-    love.graphics.printf("Hola Pongo!", 0, 10, VIRTUAL_WIDTH, 'center')
+    -- love.graphics.printf("Hola Pongo!", 0, 10, VIRTUAL_WIDTH, 'center')
+    if gameState == 'start' then
+        love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
 
     love.graphics.setFont(scoreFont)
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 8)
     love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 8)
 
-    love.graphics.setColor(249/255, 226/255, 175/255)
+    love.graphics.setColor(249 / 255, 226 / 255, 175 / 255)
     love.graphics.rectangle('fill', 10, player1Y, 5, 20)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5, 20)
 
-    love.graphics.setColor(243/255, 139/255, 168/255)
-    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 , VIRTUAL_HEIGHT / 2 , 4, 4)
+    love.graphics.setColor(243 / 255, 139 / 255, 168 / 255)
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 4, 4)
 
     push:apply('end')
 end
